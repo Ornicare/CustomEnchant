08a09a16ae395161adb197107645c8e53913e49a
Nicnl Trololo <nicnl25@gmail.com>
2013-03-05 08:17:05 +0100

    fix

--- CustomEnchant/src/fr/enchantments/custom/factory/ListenerRegistrationFactory.java
+++ CustomEnchant/src/fr/enchantments/custom/factory/ListenerRegistrationFactory.java
@@ -4,17 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import fr.enchantments.custom.model.*;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 import fr.enchantments.custom.helper.EnchantmentHelper;
-import fr.enchantments.custom.model.BaseEnchantment;
-import fr.enchantments.custom.model.IArmorHitEnchantment;
-import fr.enchantments.custom.model.IDirectHitEnchantment;
-import fr.enchantments.custom.model.IEnchantment;
-import fr.enchantments.custom.model.IZoneEffectEnchantment;
 
 /**
  * That awesome class collects :
