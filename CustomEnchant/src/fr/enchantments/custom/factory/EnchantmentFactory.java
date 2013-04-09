bec02b8b2f3276d97c65a1c3b1ff776c059cb5e1
Ornicare <antoine.floriano@gmail.com>
2013-04-09 22:01:56 +0200

    Sync

--- CustomEnchant/src/fr/enchantments/custom/factory/EnchantmentFactory.java
+++ CustomEnchant/src/fr/enchantments/custom/factory/EnchantmentFactory.java
@@ -3,13 +3,13 @@ package fr.enchantments.custom.factory;
 import java.util.Map;
 
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventPriority;
 import org.bukkit.inventory.ItemStack;
 
 public class EnchantmentFactory {
 
 	public static void addNewEnchantments(ItemStack item,Map<Enchantment, Integer> enchantsToAdd, int expLevelCost) {
 		// TODO Auto-generated method stub
-		
 	}
 
 }
